46e77bd2bfb4620e2d0e4b8d33dd367c5ae3c7b3
ZephyrII <tomasz.no@op.pl>
2020-03-27 08:37:25 +0100

    OS1-128

--- main.cpp
+++ main.cpp
@@ -14,10 +14,10 @@ void chatterCallback(const sensor_msgs::PointCloud2::ConstPtr &cloudMsg) {
     pcl::fromROSMsg(*cloudMsg, *cloud);
 
 
-    int SCANNER_COLS = 2083;
-    int SCANNER_ROWS = 64;
-    float UPPER_BOUND = 2.0;
-    float LOWER_BOUND = -24.3;
+    int SCANNER_COLS = 2048;
+    int SCANNER_ROWS = 128;
+    float UPPER_BOUND = 21.5;
+    float LOWER_BOUND = -21.5;
     float VELO_FACTOR_VER = (SCANNER_ROWS - 1) / (UPPER_BOUND - LOWER_BOUND);
     float VELO_FACTOR_HOR = (SCANNER_COLS - 1) / 360.0;
 
@@ -55,7 +55,7 @@ void chatterCallback(const sensor_msgs::PointCloud2::ConstPtr &cloudMsg) {
         // Filling depth image
         depthImg.at<float>(row, col) = val;
     }
-    cv::imshow("lol", depthImg);
+    cv::imshow("lol", depthImg/255.0);
 //    int k = cv::waitKey(10);
 //    if (k == 27) {
 //        exit(0);
@@ -64,25 +64,144 @@ void chatterCallback(const sensor_msgs::PointCloud2::ConstPtr &cloudMsg) {
 
     // Original data from ouster is 1024 x 64
     int wantedWidth = 1024, wantedHeight = 512;
-    double fx = 880, fy = 880;
+    double fx = 510, fy = 510;
 
-    std::vector<double> beamAngles = {-24.3, -23.8, -23.3, -22.8, -22.3, -21.8, -21.3, -20.8, -20.3, -19.8, -19.3,
-                                      -18.8, -18.3, -17.8, -17.3, -16.8, -16.3, -15.8, -15.3, -14.8, -14.3, -13.8,
-                                      -13.3, -12.8, -12.3, -11.8, -11.3, -10.8, -10.3, -9.8, -9.3, -8.8,
-                                      -8.3333333, -8.0, -7.666666, -7.3333333, -7.0,
-                                      -6.66666666666, -6.3333333333, -6.0, -5.6666667, -5.3333333,
-                                      -5.0, -4.6666666667, -4.333333, -4.0, -3.666666667,
-                                      -3.333333333333, -3.0, -2.66666667, -2.3333333333, -2.0,
-                                      -1.6666666667, -1.3333333333, -1.0, -0.66666666667, -0.333333333333333,
-                                      0.0, 0.33333333334, 0.66666666667, 1.0, 1.333333333, 1.6666666667, 2.0};
+    std::vector<double> beamAngles = {21.4764,
+                                      21.1679,
+                                      20.8583,
+                                      20.5477,
+                                      20.2356,
+                                      19.9221,
+                                      19.6075,
+                                      19.2919,
+                                      18.975,
+                                      18.6567,
+                                      18.3375,
+                                      18.0171,
+                                      17.6955,
+                                      17.3729,
+                                      17.0492,
+                                      16.7242,
+                                      16.3982,
+                                      16.0716,
+                                      15.7437,
+                                      15.4141,
+                                      15.0841,
+                                      14.7537,
+                                      14.4218,
+                                      14.088,
+                                      13.754,
+                                      13.4202,
+                                      13.0844,
+                                      12.7466,
+                                      12.409,
+                                      12.0718,
+                                      11.7326,
+                                      11.3909,
+                                      11.0499,
+                                      10.7097,
+                                      10.3671,
+                                      10.022,
+                                      9.6778,
+                                      9.3348,
+                                      8.9892,
+                                      8.641,
+                                      8.2939,
+                                      7.9482,
+                                      7.5999,
+                                      7.249,
+                                      6.8994,
+                                      6.5512,
+                                      6.2005,
+                                      5.8474,
+                                      5.4956,
+                                      5.1452,
+                                      4.7925,
+                                      4.4377,
+                                      4.0842,
+                                      3.7318,
+                                      3.3775,
+                                      3.0216,
+                                      2.6668,
+                                      2.3127,
+                                      1.9573,
+                                      1.601,
+                                      1.2454,
+                                      0.89,
+                                      0.5341,
+                                      0.1779,
+                                      -0.1779,
+                                      -0.5341,
+                                      -0.89,
+                                      -1.2454,
+                                      -1.601,
+                                      -1.9573,
+                                      -2.3127,
+                                      -2.6668,
+                                      -3.0216,
+                                      -3.3775,
+                                      -3.7318,
+                                      -4.0842,
+                                      -4.4377,
+                                      -4.7925,
+                                      -5.1452,
+                                      -5.4956,
+                                      -5.8474,
+                                      -6.2005,
+                                      -6.5512,
+                                      -6.8994,
+                                      -7.249,
+                                      -7.5999,
+                                      -7.9482,
+                                      -8.2939,
+                                      -8.641,
+                                      -8.9892,
+                                      -9.3348,
+                                      -9.6778,
+                                      -10.022,
+                                      -10.3671,
+                                      -10.7097,
+                                      -11.0499,
+                                      -11.3909,
+                                      -11.7326,
+                                      -12.0718,
+                                      -12.409,
+                                      -12.7466,
+                                      -13.0844,
+                                      -13.4202,
+                                      -13.754,
+                                      -14.088,
+                                      -14.4218,
+                                      -14.7537,
+                                      -15.0841,
+                                      -15.4141,
+                                      -15.7437,
+                                      -16.0716,
+                                      -16.3982,
+                                      -16.7242,
+                                      -17.0492,
+                                      -17.3729,
+                                      -17.6955,
+                                      -18.0171,
+                                      -18.3375,
+                                      -18.6567,
+                                      -18.975,
+                                      -19.2919,
+                                      -19.6075,
+                                      -19.9221,
+                                      -20.2356,
+                                      -20.5477,
+                                      -20.8583,
+                                      -21.1679,
+                                      -21.4764};
 
     double cx = wantedWidth / 2.0, cy = wantedHeight / 2.0;
 
     // TODO: Create map_x and map_y that for wanted image produces indicies in the original image
-    for (int imagePart = 0; imagePart < 6; imagePart++) {
+    for (int imagePart = 0; imagePart < 4; imagePart++) {
 
         // We create 6 images so we move by 60 deg
-        double thetaIncrement = imagePart * M_PI / 3;
+        double thetaIncrement = 2*imagePart * M_PI / 4;
 
         cv::Mat map_x = cv::Mat(wantedHeight, wantedWidth, CV_32FC1), map_y = cv::Mat(wantedHeight, wantedWidth,
                                                                                       CV_32FC1);
@@ -104,10 +223,10 @@ void chatterCallback(const sensor_msgs::PointCloud2::ConstPtr &cloudMsg) {
                     theta = theta + 2 * M_PI;
 
                 // Horizontal angle to index
-                double originalU = theta * 180.0 / M_PI * 2083.0 / 360.0 + 1041.0;
+                double originalU = theta * 180.0 / M_PI * SCANNER_COLS / 360.0 + SCANNER_COLS/2-0.5;
 
                 // Vertical angle to index
-                double originalV = phi * 180.0 / M_PI * 64.0 / 26.8 + 31.5;
+                double originalV = phi * 180.0 / M_PI * SCANNER_ROWS / (UPPER_BOUND - LOWER_BOUND) + SCANNER_ROWS/2-0.5;
 
                 // Let's try finding the proper vertical index
                 double phiInDeg = phi * 180.0 / M_PI;
@@ -133,12 +252,12 @@ void chatterCallback(const sensor_msgs::PointCloud2::ConstPtr &cloudMsg) {
             }
         }
 
-        cv::Mat outputImg = cv::Mat(wantedHeight, wantedWidth, CV_8UC1, cv::Scalar(0,0,0));
+        cv::Mat outputImg = cv::Mat(wantedHeight, wantedWidth, CV_8UC1, cv::Scalar(0, 0, 0));
 //        cv::Mat inpainted = cv::Mat(wantedHeight, wantedWidth, CV_8UC1, cv::Scalar(0,0,0));
 //        cv::Mat mask = cv::Mat(wantedHeight, wantedWidth, CV_8UC1, cv::Scalar(0,0,0));
         cv::remap(depthImg, outputImg, map_x, map_y, cv::INTER_LINEAR, cv::BORDER_CONSTANT, cv::Scalar());
         std::string filename =
-                "/root/share/tf/dataset/kitti_intensity_val_05/" + std::to_string(imagePart) + "_" +
+                "/root/share/tf/dataset/os1_128_intensity/" + std::to_string(imagePart) + "_" +
                 std::to_string(cloudMsg->header.stamp.toSec()) + ".png";
 //        std::cout<<filename<<std::endl;
 //        cv::cvtColor(outputImg, outputImg, cv::COLOR_BGR2GRAY);
@@ -148,8 +267,8 @@ void chatterCallback(const sensor_msgs::PointCloud2::ConstPtr &cloudMsg) {
 //        cv::imshow("lol2", mask*255);
 //        cv::GaussianBlur(outputImg, outputImg, CvSize(5,5), 0);
 //        cv::inpaint(outputImg, mask, inpainted, 3, cv::INPAINT_TELEA);
-        cv::imshow("lol1", outputImg);
-//        cv::imwrite(filename, outputImg*255);
+        cv::imshow("lol1", outputImg/255.0);
+//        cv::imwrite(filename, outputImg);
         int k = cv::waitKey(0);
         if (k == 27) {
             exit(0);
